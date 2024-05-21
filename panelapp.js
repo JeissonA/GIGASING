847f7c05c31e0ccdd7e7289fa90f577254b48cc2
JeissonA <110418160+JeissonA@users.noreply.github.com>
2024-05-21 16:29:57 -0500

    Add files via upload

--- panelapp.js
+++ panelapp.js
@@ -73,16 +73,19 @@ document.addEventListener("DOMContentLoaded", function() {
   const formSections = document.querySelectorAll(".form-section");
   const nextButtons = document.querySelectorAll(".next-btn");
   const prevButtons = document.querySelectorAll(".prev-btn");
+  const progressBar = document.getElementById("progress-bar");
 
   let currentSection = 0;
 
   formSections[currentSection].classList.add("active");
+  updateProgressBar();
 
   nextButtons.forEach((button, index) => {
       button.addEventListener("click", () => {
           formSections[currentSection].classList.remove("active");
           currentSection = Math.min(currentSection + 1, formSections.length - 1);
           formSections[currentSection].classList.add("active");
+          updateProgressBar();
       });
   });
 
@@ -91,6 +94,7 @@ document.addEventListener("DOMContentLoaded", function() {
           formSections[currentSection].classList.remove("active");
           currentSection = Math.max(currentSection - 1, 0);
           formSections[currentSection].classList.add("active");
+          updateProgressBar();
       });
   });
 
@@ -98,7 +102,13 @@ document.addEventListener("DOMContentLoaded", function() {
       event.preventDefault();
       alert("Formulario enviado!");
   });
+
+  function updateProgressBar() {
+      const progress = (currentSection + 1) / formSections.length * 100;
+      progressBar.style.width = progress + "%";
+  }
 });
+
 
 //ANUNCIOS//
 document.addEventListener('DOMContentLoaded', function () {
@@ -146,6 +156,29 @@ function guardarDatos() {
   var permiso_hasta = document.getElementById('permiso_hasta').value;
   var hora_inicio = document.getElementById('hora_inicio').value;
   var hora_fin = document.getElementById('hora_fin').value;
+  var techox = document.querySelector('input[name="techox"]:checked').value;
+ var plataformas = document.querySelector('input[name="plataformas"]:checked').value;
+ var gruas = document.querySelector('input[name="gruas"]:checked').value;
+ var andamios = document.querySelector('input[name="andamios"]:checked').value;
+ var malacates = document.querySelector('input[name="malacates"]:checked').value;
+ var otros = document.querySelector('input[name="otros"]:checked').value;
+ var especifica = document.getElementById('especifica').value;
+ var escaleras = document.querySelector('input[name="escaleras"]:checked').value;
+ var moviles = document.querySelector('input[name="moviles"]:checked').value;
+ var canasta = document.querySelector('input[name="canasta"]:checked').value;
+ var andamiox = document.querySelector('input[name="andamiox"]:checked').value;
+ var estructura = document.querySelector('input[name="estructura"]:checked').value;
+ var otross = document.querySelector('input[name="otross"]:checked').value;  
+ var especificar = document.getElementById('especificar').value;
+ var AyD = document.querySelector('input[name="AyD"]:checked').value;
+ var Restriccion = document.querySelector('input[name="Restriccion"]:checked').value;
+ var Posicionamiento = document.querySelector('input[name="Posicionamiento"]:checked').value;
+ var Suspension = document.querySelector('input[name="Suspension"]:checked').value;
+ var Desplazamientos = document.querySelector('input[name="Desplazamientos"]:checked').value;
+ var nombrecompleto = document.getElementById('nombrecompleto').value;
+ var CC = document.getElementById('CC').value;
+ var nocc = document.getElementById('nocc').value;
+ var cargo = document.getElementById('cargo').value;
 
   // Crear un nuevo objeto Image
   var img = new Image();
@@ -173,7 +206,30 @@ function guardarDatos() {
           fecha_expedicion: { x: 902, y: 165 },
           permiso_hasta: { x: 348, y: 180 },
           hora_inicio: { x: 755, y: 180 },
-          hora_fin: { x: 958, y: 180 }
+          hora_fin: { x: 958, y: 180 },
+          techox: { x: 180, y: 235 },
+          plataformas: { x: 286, y: 235 },
+          gruas: { x: 416, y: 255 },
+          andamios: { x: 416, y: 272 },
+          malacates: { x: 416, y: 290 },
+          otros: { x: 196, y: 310 },
+          especifica: { x: 547, y: 230 },
+          escaleras: { x: 551, y: 234 },
+          moviles: { x: 650, y: 234 },
+          canasta: { x: 788, y: 252 },
+          andamiox: { x: 788, y: 271 },
+          estructura: { x: 788, y: 289 },
+          otross: { x: 495, y: 310 },
+          especificar: { x: 530, y: 310 },
+          AyD: { x: 1092, y: 237 },
+          Restriccion: { x: 1092, y: 253 },
+          Posicionamiento: { x: 1092, y: 270 },
+          Suspension: { x: 1092, y: 287 },
+          Desplazamientos: { x: 1092, y: 307 },
+          nombrecompleto: { x: 196, y: 455 },
+          CC: { x: 548, y: 455 },
+          nocc: { x: 682, y: 455 },
+          cargo: { x: 792, y: 455 },
       };
 
       // Superponer los datos del formulario en las coordenadas especificadas
@@ -183,6 +239,29 @@ function guardarDatos() {
       ctx.fillText(permiso_hasta, coordenadas.permiso_hasta.x, coordenadas.permiso_hasta.y);
       ctx.fillText(hora_inicio, coordenadas.hora_inicio.x, coordenadas.hora_inicio.y);
       ctx.fillText(hora_fin, coordenadas.hora_fin.x, coordenadas.hora_fin.y);
+      ctx.fillText(techox, coordenadas.techox.x, coordenadas.techox.y);
+     ctx.fillText(plataformas, coordenadas.plataformas.x, coordenadas.plataformas.y);
+     ctx.fillText(gruas, coordenadas.gruas.x, coordenadas.gruas.y);
+     ctx.fillText(andamios, coordenadas.andamios.x, coordenadas.andamios.y);
+     ctx.fillText(malacates, coordenadas.malacates.x, coordenadas.malacates.y);
+     ctx.fillText(otros, coordenadas.otros.x, coordenadas.otros.y);
+     ctx.fillText(especifica, coordenadas.especifica.x, coordenadas.especifica.y);
+     ctx.fillText(escaleras, coordenadas.escaleras.x, coordenadas.escaleras.y);
+     ctx.fillText(moviles, coordenadas.moviles.x, coordenadas.moviles.y);
+     ctx.fillText(canasta, coordenadas.canasta.x, coordenadas.canasta.y);
+     ctx.fillText(andamiox, coordenadas.andamiox.x, coordenadas.andamiox.y);
+     ctx.fillText(estructura, coordenadas.estructura.x, coordenadas.estructura.y);
+     ctx.fillText(otross, coordenadas.otross.x, coordenadas.otross.y);
+     ctx.fillText(especificar, coordenadas.especificar.x, coordenadas.especificar.y);
+     ctx.fillText(AyD, coordenadas.AyD.x, coordenadas.AyD.y);
+     ctx.fillText(Restriccion, coordenadas.Restriccion.x, coordenadas.Restriccion.y);
+     ctx.fillText(Posicionamiento, coordenadas.Posicionamiento.x, coordenadas.Posicionamiento.y);
+     ctx.fillText(Suspension, coordenadas.Suspension.x, coordenadas.Suspension.y);
+     ctx.fillText(Desplazamientos, coordenadas.Desplazamientos.x, coordenadas.Desplazamientos.y);
+     ctx.fillText(nombrecompleto, coordenadas.nombrecompleto.x, coordenadas.nombrecompleto.y);
+     ctx.fillText(CC, coordenadas.CC.x, coordenadas.CC.y);
+     ctx.fillText(nocc, coordenadas.nocc.x, coordenadas.nocc.y);
+     ctx.fillText(cargo, coordenadas.cargo.x, coordenadas.cargo.y);
 
       // Convertir el canvas a imagen JPEG
       var imagenDataUrl = canvas.toDataURL('image/jpeg');
